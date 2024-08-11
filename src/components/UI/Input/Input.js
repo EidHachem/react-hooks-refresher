0f5bb0575c113d24eac9e6e898ea178d06c233d7
eid <eidhachem1@gmail.com>
2024-08-11 21:39:19 +0300

    Add useImparetiveRef hook to control input component status

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,24 +1,38 @@
-import React from "react"
+import React, { useRef, useImperativeHandle } from "react"
 
 import classes from "./Input.module.css"
 
-const Input = ({ isValid, label, id, value, type, onChange, onBlur }) => {
-  return (
-    <div
-      className={`${classes.control} ${
-        isValid === false ? classes.invalid : ""
-      }`}
-    >
-      <label htmlFor="password">{label}</label>
-      <input
-        type={type}
-        id={id}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
-      />
-    </div>
-  )
-}
+const Input = React.forwardRef(
+  ({ isValid, label, id, value, type, onChange, onBlur }, ref) => {
+    const inputRef = useRef()
+
+    const activate = () => {
+      inputRef.current.focus()
+    }
+
+    useImperativeHandle(ref, () => {
+      return {
+        focus: activate,
+      }
+    })
+    return (
+      <div
+        className={`${classes.control} ${
+          isValid === false ? classes.invalid : ""
+        }`}
+      >
+        <label htmlFor="password">{label}</label>
+        <input
+          ref={inputRef}
+          type={type}
+          id={id}
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+      </div>
+    )
+  }
+)
 
 export default Input
